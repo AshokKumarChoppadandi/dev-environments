4af804e3c685396ade0c3fffbf85ce7f66c9e65f
AshokKumarChoppadandi <ashokkumar98778@gmail.com>
2020-06-25 00:12:15 +0530

    #9 Maven Docker Hello World project added

---
+++ MavenDockerHelloWorld/src/main/java/com/bigdata/docker/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.bigdata.docker;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello " + args[0] + "...!!!");
+        }
+    }
+}
